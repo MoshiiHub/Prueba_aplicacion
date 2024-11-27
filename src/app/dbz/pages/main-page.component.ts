801181d5ec91d30fb0c958611bfa34efc46c85ba
MoshiiHub <mirian.jmz@gmail.com>
2024-11-27 20:33:04 +0100

    Nuevos cambios

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -3,6 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
 
 })
 
